c6ff7a7181cead1803de0cb250b19997730e7759
looninho <looninho@gmail.com>
2023-02-04 17:22:18 +0100

    first Pn00x implementation

--- ac_servo_rtu_backend.py
+++ ac_servo_rtu_backend.py
@@ -61,15 +61,82 @@ class AASD_15A(object):
         self.UNIT = slave_address
         self.controller = client
         
-    def Pn001(self):
-        pass
+    # System parameters
+    def get_parameter_editing_mode(self) -> int:
+        """This is the Pn000 reading function. It returns the current 
+        parameter editing mode.
+
+        Returns:
+            int: current parameter editing mode:
+                0: Parameter initialization prohibited.
+                1: allow parameter initialization but does not initialize
+                  Pn001, 80, 159, 190 and other application independent 
+                  functional parameters.
+                2: restore settings before shipment.
+                3: press button to view mode and cannot modify parameters.
+        """        
+        rr = self.controller.read_holding_registers(0, 1, unit=self.UNIT) # 0 for Pn000, 1 for number of word(s) to read
+        return rr.getRegister(0) # O is the LSB, 1 is MSB
+    
+    def set_parameter_editing_mode(self, value:int) -> int:
+        """This is the Pn000 writing function.
+
+        Args:
+            value (int): 0, 1, 2 or 3 see descriptions above.
+
+        Returns:
+            int: see above
+        """        
+        rq = self.controller.write_register(0, int(value), unit=self.UNIT) # 0 is Pn000
+        return self.get_parameter_editing_mode() == value
+    
+    def get_motor_code(self) -> int:
+        """This is Pn001 function for reading.
+
+        Returns:
+            int: 0..35: the current motor code. For 80ST-M02430 the code is 4.
+        """        
+        rr = self.controller.read_holding_registers(1, 1, unit=self.UNIT) #  for Pn001, 1 or 2? for number of word(s) to read
+        return rr.getRegister(0)
+    
+    def set_motor_code(self, value:int) -> int:
+        """This is the Pn000 writing function.
+
+        Args:
+            value (int): 0..35 see descriptions above.
+
+        Returns:
+            int: TODO
+        """        
+        rq = self.controller.write_register(0, int(value), unit=self.UNIT)
+        return self.get_motor_code() == value
+    
+    #TODO: for other functions that we don't need return value, we can ignore the return function to spped up the communication.
+
 
 if __name__ == "__main__":
+    # initialize de COM port and hook it to the driver
     client = ModbusClient(method='rtu', 
                                 port='COM2', 
                                 timeout=1, 
                                 baudrate=9600, 
                                 strict=False)
     
-    servo = AASD_15A(client, 1) # address = 1
+    servo = AASD_15A(client, 1) # address = 1 for motor 1, 2 for motor 2, etc.
+    
+    # let's see what is the current initialization mode:
+    print(f"the current parameter initilization mode is: {servo.get_parameter_editing_mode()}")
+    
+    # now we want to allow parameter initialization, we enter the value 1 for that:
+    servo.set_parameter_editing_mode(1)
+    
+    # set the motor code for your servo motor. For 80ST_M02430, the code is 4:
+    servo.set_motor_code(4)
+    
+    # When you finish edition, don't forget to put it back to value 3
+    servo.set_parameter_editing_mode(3)
+    
+    
+    
+    
     
